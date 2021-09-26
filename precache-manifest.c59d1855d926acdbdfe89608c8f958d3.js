344b058fc16ca6894ff88669fd55214aab46a0f4
Nithin47TA <nithinta234@gmail.com>
2021-09-26 10:19:16 +0530

    Updates

---
+++ precache-manifest.c59d1855d926acdbdfe89608c8f958d3.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "88b90f78013dc12feab399865570adb2",
+    "url": "/Keep-Clone/index.html"
+  },
+  {
+    "revision": "9572dcccd0ec34dc9b87",
+    "url": "/Keep-Clone/static/js/2.3a2d6c44.chunk.js"
+  },
+  {
+    "revision": "acd5da62d51a74c92e29",
+    "url": "/Keep-Clone/static/js/main.5b17dc91.chunk.js"
+  },
+  {
+    "revision": "14d4d64c9ceb66b59996",
+    "url": "/Keep-Clone/static/js/runtime-main.915096c1.js"
+  }
+]);
